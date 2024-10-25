a1ed073311403cb4933c1a7ccadf522249fe80d1
김혜영 <sunzero0116@gmail.com>
2024-10-25 16:31:10 +0900

    refactor : Answer에 delete 기능 추가

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,26 @@
 package nextstep.qna.domain;
 
+import nextstep.qna.CannotDeleteException;
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    void delete_성공() throws Exception {
+        A1.delete(NsUserTest.JAVAJIGI);
+        assertThat(A1.isDeleted()).isTrue();
+    }
+
+    @Test
+    void delete_다른_사람이_쓴_글() throws Exception {
+        assertThatThrownBy(() -> {
+            A1.delete(NsUserTest.SANJIGI);
+        }).isInstanceOf(CannotDeleteException.class);
+    }
 }
